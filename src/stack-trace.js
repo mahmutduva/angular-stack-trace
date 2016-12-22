459e7b8c87b9fdbc464c9e64b41ee9fd6cdf0303
Mahmut Duva <mahmutduva@gmail.com>
2016-12-22 11:13:03 +0300

    add stack trace to dependencies

--- src/stack-trace.js
+++ src/stack-trace.js
@@ -1,69 +1,49 @@
-'use strict';
+'use strict'
 
 angular
     .module('stackTrace', [])
     .factory('StackTraceService', StackTraceService)
     .factory('ErrorLogService', ErrorLogService)
-    .provider('$exceptionHandler', ExceptionHandler);
-
-
-function StackTraceService(){
-  return({
-      print: printStackTrace
-  });
-}
-
-function ExceptionHandler(){
-  $get: function( errorLogService ) {
-      return( errorLogService );
-
-  }
-}
-
-
-
-function ErrorLogService($log, $window, StackTraceService, ENV, $cookies){
-    function log( exception, cause ) {
-
-      $log.error.apply( $log, arguments );
-
-      try {
-
-        var errorMessage = exception.toString();
-        var stackTrace = StackTraceService.print({ e: exception });
-
-        $.ajax({
-          type: 'POST',
-          url: ENV.logUrl,
-          contentType: 'application/json',
-          headers: {
-            'X-Authorization' : $cookies.get('_token')
-          },
-          data: angular.toJson({
-            errorUrl: $window.location.href,
-            errorMessage: errorMessage,
-            stackTrace: stackTrace,
-            cause: ( cause || '' )
-          })
-        });
-
-      } catch ( loggingError ) {
-
-        $log.warn( 'Error logging failed' );
-        $log.log( loggingError );
-
+    .provider('$exceptionHandler', {
+      $get: function (ErrorLogService) {
+        return ErrorLogService
       }
+    })
 
-    }
-
-    return( log );
+function StackTraceService () {
+  return ({
+    print: printStackTrace
+  })
 }
 
+function ErrorLogService ($log, $window, StackTraceService, ENV, $cookies) {
+  function log (exception, cause) {
+    $log.error.apply($log, arguments)
 
+    try {
+      var errorMessage = exception.toString()
+      var stackTrace = StackTraceService.print({ e: exception })
+      var stackTraceUrl = ENV.apiCockpit.concat(ENV.stackTracePath || '/fronend/log')
 
+      $.ajax({
+        type: 'POST',
+        url: stackTraceUrl,
+        headers: {
+          'Authorization': 'Bearer' + $cookies.get('_token')
+        },
+        data: angular.toJson({
+          errorUrl: $window.location.href,
+          errorMessage: errorMessage,
+          stackTrace: stackTrace,
+          cause: (cause || '')
+        })
+      })
+    } catch (loggingError) {
+      $log.warn('Error logging failed')
+      $log.log(loggingError)
+    }
+  }
 
-
-
-
-
+  return (log)
+}
 
